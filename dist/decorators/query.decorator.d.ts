996e734a75aaa78182ce8e0f8a6a2fc1abc19778
xinshangshangxin <xinshangshangxin@gmail.com>
2019-12-24 14:47:31 +0800

    chore: dist

---
+++ dist/decorators/query.decorator.d.ts
@@ -0,0 +1,10 @@
+import {
+  AdvancedOptions,
+  ReturnTypeFunc,
+} from './../external/type-graphql.types';
+export declare function Query(): MethodDecorator;
+export declare function Query(name: string): MethodDecorator;
+export declare function Query(
+  typeFunc: ReturnTypeFunc,
+  options?: AdvancedOptions,
+): MethodDecorator;
